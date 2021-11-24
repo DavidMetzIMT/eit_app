97bddf33f5dad46818a9f088d3321637d55b5bed
David Metz <david.metz67@gmail.com>
2021-11-24 10:15:04 +0100

    update batch file

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
 E:
-cd E:\EIT_Project\05_Engineering\04_Software\Python\eit_app
-conda activate app2
+cd E:\Software_dev\Python\eit_app
+conda activate tf-app
 pyuic5 -x eit_app/app/gui.ui -o eit_app/app/gui.py
 python eit_app/main.py
+
 
 
 
