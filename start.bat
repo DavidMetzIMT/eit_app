18759a732781e671bcb23768919b2be719f547f1
David Metz <david.metz67@gmail.com>
2022-03-21 19:11:24 +0100

    fixing path

--- start.bat
+++ start.bat
@@ -2,8 +2,8 @@ E:
 cd E:\Software_dev\Python\eit_app
 conda activate ai-app
 python eit_app/main.py
-pyuic5 -x eit_appgui.ui -o eit_appgui.py
-pyrcc5 eit_appresource.qrc -o eit_app/resource_rc.py
+pyuic5 -x eit_app/gui.ui -o eit_app/gui.py
+pyrcc5 eit_app/resource.qrc -o eit_app/resource_rc.py
 
 
 
