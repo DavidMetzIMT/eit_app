37b69bce028161bbd2335f33eb93593a88c1da27
David Metz <david.metz67@gmail.com>
2022-04-19 11:10:49 +0200

    new gui layout

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 E:
 cd E:\Software_dev\Python\eit_app
 conda activate ai-app
-python eit_app/main.py
 pyuic5 -x eit_app/gui.ui -o eit_app/gui.py
 pyrcc5 eit_app/resource.qrc -o eit_app/resource_rc.py
+python eit_app/main.py
 
 
 
